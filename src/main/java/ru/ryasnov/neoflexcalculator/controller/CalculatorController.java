30962162372a52efec064e15a752f9d18e1cb0ae
Blooden <pashpavel217@gmail.com>
2024-03-28 15:07:33 +0300

    Fix bugs when moving from 17 to 11 version

--- src/main/java/ru/ryasnov/neoflexcalculator/controller/CalculatorController.java
+++ src/main/java/ru/ryasnov/neoflexcalculator/controller/CalculatorController.java
@@ -1,6 +1,7 @@
 package ru.ryasnov.neoflexcalculator.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ import java.time.LocalDate;
 public class CalculatorController {
     private final CalculatorService calculatorService;
     @GetMapping("/calculacte")
-    public ResponseEntity<Integer> calculating(@RequestParam(required = false) LocalDate dateOfStart,
+    public ResponseEntity<Integer> calculating(@RequestParam(required = false)@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dateOfStart,
                                                @RequestParam Integer countOfDays, @RequestParam Integer averageSalary) throws CalcException{
         return new ResponseEntity<>(calculatorService.calculating(dateOfStart, countOfDays, averageSalary), HttpStatus.OK);
     }
